8c64744559032eb8c7ec086dac61b511bb7d7562
PK Banks <pkbanks12@gmail.com>
2019-02-17 12:15:12 -0500

    AWS

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,15 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+        provider:              'AWS',
+        aws_access_key_id:     Rails.application.credentials.aws['S3_KEY'],
+        aws_secret_access_key: Rails.application.credentials.aws['S3_SECRET'],
+        region:                Rails.application.credentials.aws['S3_REGION'],
+    }
+    config.fog_directory  = Rails.application.credentials.aws['S3_BUCKET']
+  else
+    config.storage = :file
+    config.enable_processing = Rails.env.development?
+  end
+end
